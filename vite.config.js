5ce6d21684948b877b81ae568cff438ecc43b085
Maria Bote <enbotedebote@gmail.com>
2024-04-29 10:28:06 +0200

    Color schema changed

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,31 @@ import { defineConfig } from "vite";
 // html partals
 import injectHTML from "vite-plugin-html-inject";
 // optimize images
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 // Calculate paths
-import FastGlob from 'fast-glob'
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import FastGlob from "fast-glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // Get all html files
 const htmlFilesList = Object.fromEntries(
-  FastGlob.sync('src/*.html').map(file => [
+  FastGlob.sync("src/*.html").map((file) => [
     // This remove `src/` as well as the file extension from each
     // file, so e.g. src/nested/foo.js becomes nested/foo
     path.relative(
-      'src',
+      "src",
       file.slice(0, file.length - path.extname(file).length)
     ),
     // This expands the relative paths to absolute paths, so e.g.
     // src/nested/foo becomes /project/src/nested/foo.js
-    fileURLToPath(new URL(file, import.meta.url))
-  ]));
+    fileURLToPath(new URL(file, import.meta.url)),
+  ])
+);
 
 const inputFilesList = {
   ...htmlFilesList,
-  'main': 'src/js/main.js',
-}
+  main: "src/js/main.js",
+};
 
 export default defineConfig({
   base: "./",
@@ -41,9 +42,9 @@ export default defineConfig({
       input: inputFilesList,
       output: {
         sourcemap: true,
-        entryFileNames: ({name}) => {
-          if( name === 'main' ) {
-            return 'js/main.js';
+        entryFileNames: ({ name }) => {
+          if (name === "main") {
+            return "js/main.js";
           }
           // default value
           // ref: https://rollupjs.org/configuration-options/#output-entryfilenames
@@ -54,6 +55,7 @@ export default defineConfig({
   },
   server: {
     open: "/index.html",
+    watch: { usePolling: true },
   },
   plugins: [
     injectHTML(),
